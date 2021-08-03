28fe42f686a66c0e52567e7005655077480b37f5
Michelle Nguyen <michellenguyen11239@aol.com>
2021-08-02 17:03:06 -0700

    Added inquirer menu prompts and added userChoice function in index.js to redirect the menu

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
 /* This file is responsible for controlling the overall flow of the application */
+
+// Import inquirer, sequelize connection, book model, and inquirer prompts
+const inquirer = require('inquirer');
+const sequelize = require('./config/connection');
+const Book = require('./models/Book');
+const menu = require('./src/inquirer');
+
+const startPrompts = () => {
+    inquirer.prompt(menu).then(answers => {
+        userChoice(answers.choice);
+    }).catch(error => {
+        if (error.isTtyError) {
+            console.log("The prompt couldn't be rendered in this current environment.");
+        } else {
+            console.log("Something else went wrong");
+        }
+    })
+}
+
+// Redirects the menu based on the user's input
+const userChoice = (choice) => {
+    switch (choice) {
+        case '1) View all books':
+            console.log('*View all books');
+            break;
+        case '2) Add a book': 
+            console.log('*Add a book');
+            break;
+        case '3) Edit a book': 
+            console.log('*Edit a book');
+            break;
+        case '4) Search for a book':
+            console.log('*Search for a book');
+            break;
+        case '5) Save and exit':
+            console.log('Save and exit');
+            break;
+        default: 
+            console.log('There was an error.');
+    }
+}
+
+// Connects to the db and starts the prompts
+sequelize.sync({ force: false }).then(() => {
+    console.log('DB connection is successful.\n');
+    startPrompts();
+}, (error) => {
+    console.log(`There was an issue with the DB connection: ${error}`);
+});
--- src/inquirer.js
+++ src/inquirer.js
@@ -1,1 +1,22 @@
 /* Inquirer prompts */
+
+// Import dependencies
+const inquirer = require('inquirer');
+
+// Menu choices
+const menu = [ 
+    { 
+        name: 'choice',
+        message: '==== Book Manager ====\n',
+        type: 'list',
+        choices: [ 
+            "1) View all books",
+            "2) Add a book",
+            "3) Edit a book",
+            "4) Search for a book", 
+            "5) Save and exit", 
+        ]   
+    }
+]
+
+module.exports = menu;
